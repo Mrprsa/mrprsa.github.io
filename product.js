e38ab72edad751e38c7a8a7b8bd261b321c90e77
Mrprsa <132891001+Mrprsa@users.noreply.github.com>
2025-04-09 04:17:01 +0400

    test

--- product.js
+++ product.js
@@ -1,6 +1,16 @@
-cart = 0;
+let productName = 'anything';
+let product = 0;
+let cart = 0;
+// Function to add a product
 function addToCart() {
-    cart += 1;
-    console.log(`Product added to cart. Total items in cart: ${cart}`);
+    product += 1;
+    console.log(`Product added to cart. Total items in cart: ${product}`);
+    cart = product;
     return cart;
+}
+
+// Function to show cart information
+
+function showCart() {
+    console.log(`Total items in cart: ${cart}`);
 }
